ae8aaad260f892b4d496227ba3e8a4f3fbbffe45
jennylea28 <jenny.lea168@btinternet.com>
2016-02-20 20:45:37 +0000

    table

--- app/handlers/index.js
+++ app/handlers/index.js
@@ -1,9 +1,15 @@
 import Router from 'express-promise-router';
+import Octonode from 'octonode';
+import Q from 'q';
 
 const router = Router();
 export default router;
 
 // prefix /
-router.get('/', function (req, res) {
-	res.render('home');
+router.get('/', async function (req, res) {
+	const client = Octonode.client(req.auth.githubToken);
+	const repos = await Q.ninvoke(client.me(), 'repos');
+	res.render('home', {
+		repos: repos
+	});
 });
