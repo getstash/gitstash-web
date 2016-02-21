326705fb360d5ad4441752f7c8565a9b9f2f8455
jennylea28 <jenny.lea168@btinternet.com>
2016-02-21 01:18:05 +0000

    fix closing bracket error

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,4 @@ $('.js-switch').bootstrapSwitch();
 $("#select-all").click(function () {
     $(".js-switch").prop('checked', $(this).prop('checked'));
     $(".js-switch").setState($(this).state());
-};
+});
