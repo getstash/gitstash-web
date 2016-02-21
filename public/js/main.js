d89d7ff8550257f09b870beb1cef2f9cce443487
Sixte de Maupeou D'Ableiges <sixtedemaupeou@gmail.com>
2016-02-21 01:26:26 +0000

    Trying to resolve time out

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ $('.js-switch').bootstrapSwitch();
 
 $("#select-all").click(function () {
     $(".js-switch").prop('checked', $(this).prop('checked'));
-    $(".js-switch").setState($(this).state());
+    //$(".js-switch").setState($(this).state());
 });
