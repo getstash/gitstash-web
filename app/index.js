4440af72b03c87bc6263c9160cdbabc872f6d46e
jennylea28 <jenny.lea168@btinternet.com>
2016-02-21 10:05:23 +0000

    failover

--- app/index.js
+++ app/index.js
@@ -10,11 +10,17 @@ app.engine('handlebars', exphbs({
 	defaultLayout: 'main',
 	helpers: {
 		for: function(from, to, incr, block) {
-			    var accum = '';
-			    for(var i = from; i < to; i += incr)
-			        accum += block.fn(i);
-			    return accum;
+		    var accum = '';
+		    for(var i = from; i < to; i += incr)
+		        accum += block.fn(i);
+		    return accum;
+		},
+		failover: function(...args) {
+			for(const arg of args) {
+				if (arg) 
+					return arg;
 			}
+		}
 	}
 }));
 app.set('view engine', 'handlebars');
