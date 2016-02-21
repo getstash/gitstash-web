b249c224e4993f9cb7b260bcf3308bd6b0e631b1
jennylea28 <jenny.lea168@btinternet.com>
2016-02-21 02:09:51 +0000

    toggle switch for realsies

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ $('.js-switch').bootstrapSwitch();
 
 $('#select-all').on('switchChange.bootstrapSwitch', function(event, state) {
     console.log("u changed it");
-    $(".js-switch").bootstrapSwitch('state', true, true);
+    $(".js-switch").bootstrapSwitch('state', state, state);
 });
 
 /*$("#select-all").click(function () {
